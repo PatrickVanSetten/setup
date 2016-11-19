878676365c13fc20dac56b131c311fe5038ba0e3
Michael van Setten <michaelvsetten@gmail.com>
2016-11-19 11:40:53 +0100

    Footer

--- wp-content/themes/setup/footer.php
+++ wp-content/themes/setup/footer.php
@@ -24,11 +24,11 @@
         </div>
     </div>
 </footer>
-<section id="subfooter" class="pam">
+<section id="subfooter" class="pan">
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <p>Realisatie door <a href="https://www.zekerzichtbaar.nl" target="_blank"></a></p>
+                <p>Realisatie door <a href="https://www.zekerzichtbaar.nl" target="_blank">Zeker Zichtbaar</a></p>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 social text-right">
                 Social
