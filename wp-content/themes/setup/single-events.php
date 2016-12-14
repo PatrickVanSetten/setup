0bd15669aba5ed1acc6031ad9d4063a8d45d7c21
Michael van Setten <michaelvsetten@gmail.com>
2016-12-14 19:55:49 +0100

    Events,blog & bestuur sidebar

--- wp-content/themes/setup/single-events.php
+++ wp-content/themes/setup/single-events.php
@@ -12,13 +12,53 @@ get_header(); ?>
                     <h1><?php the_title(); ?></h1>
                     <span class="date"><?php the_field('datum');?></span>
                     <span class="place">in <?php the_field('plaats');?></span>
-                    <p><?php the_content(); ?></p>
+                    <?php the_content(); ?>
                 </div>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 bg-grey pan">
                 <div class="sidebar">
-                     <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-events') ) : ?>
-                    <?php endif; ?>
+                    <h3>Bekijk ook eens</h3>
+                    <ul>
+                     <?php
+                         $args = array(
+                                'post_type' => 'events',
+                                'order' => 'DESC',
+                                'posts_per_page' => 5
+                            );
+
+                            $myposts = get_posts($args);
+                            foreach ($myposts as $post) : setup_postdata($post);
+
+                            $filter = array();
+                            $filter['title'] = strtolower( get_the_title() );
+                            $filter['datum'] = strtolower( get_field('datum') );
+                            $filter['beschrijving'] = strtolower( get_field('beschrijving') );
+                            $thumb = get_post_thumbnail_id(); 
+                            $image = vt_resize( $thumb, '', 800, 400, true );
+                            ?>
+
+                            <?php
+                                $term_list = wp_get_post_terms($post->ID, 'event-categories', array("fields" => "all"));
+
+                        ?>
+                                    <li>
+                                        <a href="<?php the_permalink(); ?>" class="">
+                                            <?php the_title(); ?>
+                                            <div class="datum">
+                                                <div class="date"><?php $date = new DateTime(get_field('datum')); echo $date->format('d'); ?></div>
+                                                <div class="month"><?php $date = new DateTime(get_field('datum')); echo $date->format('M'); ?></div>  
+                                            </div>
+                                        </a>
+                                    </li>
+                                
+
+                        <?php
+                        endforeach;
+                        echo $return;
+                        $return = '';
+                        wp_reset_postdata();
+                        ?>
+                    </ul>
                 </div>
             </div>
         </div>
