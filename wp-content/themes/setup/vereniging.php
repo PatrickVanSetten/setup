d87eed071ca3de5dc7ffb39d4fa7a9a00ce05a52
PatrickVanSetten <patrickvansetten@gmail.com>
2016-12-28 15:14:00 +0100

    Bestuurstemplate

--- wp-content/themes/setup/vereniging.php
+++ wp-content/themes/setup/vereniging.php
@@ -4,20 +4,46 @@
  */
 get_header(); ?>
 
-<section id="vereniging" class="pan content-page">
-    <div class="greybarleft widthcalc"></div>
+<section id="vereniging" class="pan bg-grey content-page">
+    <div class="whitebarleft widthcalc"></div>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 bg-grey pan">
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 bg-white pan">
                 <div class="sidebar">
                     <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('vereniging') ) : ?>
                     <?php endif; ?>
                 </div>
             </div>
-            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 pan">
-                <div class="wrapper bg-white">
+            <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+                <div class="wrapper">
                     <h1><?php the_title(); ?></h1>
-                    <p><?php the_content(); ?></p>
+                    
+                    <div class="row">
+                    <?php
+                        // check if the repeater field has rows of data
+                        if( have_rows('contactpersoon') ):
+
+                            // loop through the rows of data
+                            while ( have_rows('contactpersoon') ) : the_row();?>
+                            
+                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                <div class="contentblock bg-white">
+                                    <span><b><?php the_sub_field('titel')?></b></span>
+                                    <span><?php the_sub_field('naam')?></span>
+                                    <span><?php the_sub_field('woonplaats')?></span>
+                                    <span><?php the_sub_field('telefoon')?></span>
+                                    <a href="mailto:<?php the_sub_field('email')?>">Stuur een mail</a>
+                                </div>
+                            </div>
+                    
+                    
+                    <?php 
+                            endwhile;
+                            else :
+                            // no rows found
+                        endif;
+                    ?>
+                    </div>
                 </div>
             </div>
         </div>
